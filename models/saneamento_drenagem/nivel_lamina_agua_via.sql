c0d4a56699c2a64852f8bd00990df37a4b46ae61
patriciacatandi <patriciabcatandi@gmail.com>
2022-11-27 11:02:37 -0300

    adição da tabela nivel_lamina_agua_via

---
+++ models/saneamento_drenagem/nivel_lamina_agua_via.sql
@@ -0,0 +1,6 @@
+SELECT 
+CONCAT(id_estacao, '_', data_medicao) AS primary_key,
+SAFE_CAST(altura_agua AS FLOAT64) altura_agua,
+SAFE_CAST(DATE_TRUNC(DATE(data_medicao), day) AS TIME) horario
+SAFE_CAST(DATE_TRUNC(DATE(data_medicao), day) AS DATE) data_particao
+FROM `rj-rioaguas.saneamento_drenagem_staging.nivel_lamina_agua`
