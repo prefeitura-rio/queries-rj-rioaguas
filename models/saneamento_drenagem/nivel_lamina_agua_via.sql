7386ae7812bf4025be28e271fa45e857a710086f
patriciacatandi <patriciabcatandi@gmail.com>
2022-11-28 10:15:56 -0300

    alterando query

--- models/saneamento_drenagem/nivel_lamina_agua_via.sql
+++ models/saneamento_drenagem/nivel_lamina_agua_via.sql
@@ -1,6 +1,6 @@
 SELECT 
 CONCAT(id_estacao, '_', data_medicao) AS primary_key,
 SAFE_CAST(altura_agua AS FLOAT64) altura_agua,
-SAFE_CAST(DATE_TRUNC(DATE(data_medicao), day) AS TIME) horario
+SAFE_CAST(SAFE_CAST(data_medicao AS DATETIME) AS TIME) horario,
 SAFE_CAST(DATE_TRUNC(DATE(data_medicao), day) AS DATE) data_particao
-FROM `rj-rioaguas.saneamento_drenagem_staging.nivel_lamina_agua`
+FROM `rj-rioaguas.saneamento_drenagem_staging.nivel_lamina_agua_via`
