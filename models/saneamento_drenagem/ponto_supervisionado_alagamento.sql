40b28c2702d92186c509b301b139b5195aaf7048
patriciacatandi <patriciabcatandi@gmail.com>
2022-12-14 16:44:14 -0300

    adiçãod da base de pontos supervisionados

---
+++ models/saneamento_drenagem/ponto_supervisionado_alagamento.sql
@@ -0,0 +1,25 @@
+SELECT 
+id_ponto_supervisionado,
+classe,
+SAFE_CAST(top_50 AS INT16) top_50,
+causa_alagamento,
+medida_cor,
+eliminado,
+endereco_ponto_supervisionado,
+bairro,
+area_planejamento,
+regiao_administrativa,
+bacia_hidrografica,
+sub_bacia_hidrografica,
+SAFE_CAST(latitude AS FLOAT64) AS latitude,
+SAFE_CAST(longitude AS FLOAT64) AS longitude,
+SAFE_CAST(utm_x AS FLOAT64) AS utm_x,
+SAFE_CAST(utm_y AS FLOAT64) AS utm_y,
+ST_GEOGPOINT(SAFE_CAST(longitude AS FLOAT64), SAFE_CAST(latitude AS FLOAT64)) AS geometry,
+projeto,
+SAFE_CAST(data_atualizacao AS DATE) data_atualizacao,
+SAFE_CAST(ano_vigencia AS INT16) ano_vigencia
+FROM `rj-rioaguas.saneamento_drenagem_staging.ponto_supervisionado_alagamento
+`
+
+-- montar schema
